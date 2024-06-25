06aef0d19ad42d1cf9b5441520d26128a904f183
C0A23144 <c0a231445c@edu.teu.ac.jp>
2024-06-25 17:21:04 +0900

    演習２：複数ビーム

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -165,7 +165,8 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((300, 200))
-    beam = None
+    # beam = None
+    beams = []
     # bomb = Bomb((255, 0, 0), 10)
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
     score = Score()
@@ -177,7 +178,7 @@ def main():
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 # スペースキー押下でBeamクラスのインスタンス生成
-                beam = Beam(bird)            
+                beams.append(Beam(bird))            
         screen.blit(bg_img, [0, 0])
         
         for bomb in bombs:
@@ -192,21 +193,27 @@ def main():
                 return
         
         for i in range(len(bombs)):
-            if beam is not None:
-                if bombs[i].rct.colliderect(beam.rct):
-                    bombs[i] = None
-                    beam = None
-                    score.scr += 1
-                    bird.change_img(6, screen)
+            for j in range(len(beams)):
+                if beams[j] is not None and bombs[i] is not None:
+
+                    if bombs[i].rct.colliderect(beams[j].rct):
+                        bombs[i] = None
+                        beams[j] = None
+                        score.scr += 1
+                        bird.change_img(6, screen)
         bombs = [bomb for bomb in bombs if bomb is not None]
+        beams = [beam for beam in beams if beam is not None]
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        if beam is not None:
+        for beam in beams:
             beam.update(screen) 
+            if check_bound(beam.rct) != (True, True):
+                beams.pop(0)
         for bomb in bombs:
             bomb.update(screen)
         score.update(screen)
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
